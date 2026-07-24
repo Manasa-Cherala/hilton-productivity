9299f83aabf99bd2ecd59422cb7a2a494743d5d2
Vikyarthic-25 <vikyarthi.vikyarthi@capgemini.com>
2026-07-24 15:35:54 +0530

    Update EmployeeProductivityApplication.java

--- springboot-app/src/main/java/com/hilton/EmployeeProductivityApplication.java
+++ springboot-app/src/main/java/com/hilton/EmployeeProductivityApplication.java
@@ -1,1 +1,15 @@
+package com.hilton;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProductivityApplication{
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(EmployeeProductivityApplication.class, args);
+
+  }
+  
+}
