bab362d3bdbcbb933a2d78fac4d2ed1821f9abcf
Anshu-varma <anshu.a.varma@capgemini.com>
2026-07-24 17:05:57 +0530

    Update ProductivityController.java

--- springboot-app/src/main/java/com/hilton/controller/ProductivityController.java
+++ springboot-app/src/main/java/com/hilton/controller/ProductivityController.java
@@ -1,7 +1,7 @@
 package com.hilton.controller;
 import com.hilton.service.ProductivityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind,annotation.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductivityController{
@@ -10,7 +10,7 @@ public class ProductivityController{
 private ProductivityService service;
   
 @PostMapping("/loadEmployeeData")
-public Stirng loadData(@RequestParam String fileName) throws Exception{
+public String loadData(@RequestParam String fileName) throws Exception{
   service.loadData(fileName);
   return "Data Loaded Successfully";
   }
