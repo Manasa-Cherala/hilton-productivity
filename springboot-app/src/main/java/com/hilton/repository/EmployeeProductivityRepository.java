8cc66dcd21913d945b33b4139c464bf9e99699ae
Anshu-varma <anshu.a.varma@capgemini.com>
2026-07-24 17:14:17 +0530

    Update EmployeeProductivityRepository.java

--- springboot-app/src/main/java/com/hilton/repository/EmployeeProductivityRepository.java
+++ springboot-app/src/main/java/com/hilton/repository/EmployeeProductivityRepository.java
@@ -3,6 +3,5 @@ package com.hilton.repository;
 import com.hilton.entity.EmployeeProductivity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
 public interface EmployeeProductivityRepository extends JpaRepository<EmployeeProductivity, Long>{
 }
