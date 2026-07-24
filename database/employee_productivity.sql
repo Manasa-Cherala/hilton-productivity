f9c2d0134ca9cd78c52d560f05d40cc59dd2c1a8
Vikyarthic-25 <vikyarthi.vikyarthi@capgemini.com>
2026-07-24 15:47:00 +0530

    Update employee_productivity.sql

--- database/employee_productivity.sql
+++ database/employee_productivity.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE hiltondb;
 
+USE hiltondb;
+
+CREATE TABLE employee_productivity(
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  emp_id VARCHAR(20),
+  emp_name VARCHAR(100),
+  email VARCHAR(100),
+  productivity_hours VARCHAR(20)
+);
