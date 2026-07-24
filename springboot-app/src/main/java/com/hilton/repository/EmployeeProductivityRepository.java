7531e8d5a4c6b38f021721a63153c56a43ef41ac
Vikyarthic-25 <vikyarthi.vikyarthi@capgemini.com>
2026-07-24 16:18:56 +0530

    Update EmployeeProductivityRepository.java

--- springboot-app/src/main/java/com/hilton/repository/EmployeeProductivityRepository.java
+++ springboot-app/src/main/java/com/hilton/repository/EmployeeProductivityRepository.java
@@ -2,6 +2,7 @@ package com.hilton.repository;
 
 import com.hilton.entity.EmployeeProductivity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 @Repository
 public interface EmployeeProductivity extends JpaRepository<EmployeeProductivity, Long>{
 }
