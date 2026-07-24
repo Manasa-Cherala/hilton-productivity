50f39b8032d173b446fcd7aa8ddacfc6e63fa701
Anshu-varma <anshu.a.varma@capgemini.com>
2026-07-24 15:25:08 +0530

    Update EmployeeProductivity.java

--- springboot-app/src/main/java/com/hilton/entity/EmployeeProductivity.java
+++ springboot-app/src/main/java/com/hilton/entity/EmployeeProductivity.java
@@ -1,1 +1,62 @@
+package com.huilton.entity;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name="employee_productivity")
+public class EmployeeProductivity{
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+private String empId;
+private String empName;
+private String email;
+private String productivityHours;
+
+public EmployeeProductivity(){}
+
+public EmployeeProductivity(String empId,String empName, String email, String productivityHours){
+  this.empId=empId;
+  this.empName=empName;
+  this.email=email;
+  this.productivityHours=productivityHours;
+}
+  
+//getter
+  public Long getId(){
+    return id;
+  }
+
+  public String getEmpId(){
+    return empId;
+  }
+
+  public String getEmpName(){
+    return empName;
+  }
+
+  public String getEmail(){
+    return email;
+  }
+
+  public String getProductivityHours(){
+    return productivityHours;
+  }
+
+  //setter
+  public void setEmpId(String empId){
+    this.empId=empId;
+  }
+  
+  public void setEmpName(String empName){
+    this.empName=empName;
+  }
+
+  public void setEmail(String email){
+    this.email=email;
+  }
+
+  public void setProductivitHours(String productivityHours){
+    this.productivityHours=productivityHours;
+  }
+
+}
